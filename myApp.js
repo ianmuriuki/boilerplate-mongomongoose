b5391912025a2ecdd90a892fbd695d5f8d42209a
ianmuriuki <ianmuriuki.inc@gmail.com>
2025-06-29 18:11:28 +0300

    Clean up mongoose connection for FCC test

--- myApp.js
+++ myApp.js
@@ -8,14 +8,6 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, '❌ MongoDB connection error:'));
-db.once('open', () => {
-  console.log('✅ MongoDB connected successfully!');
-});
-
-
 // Define a schema
 const personSchema = new mongoose.Schema({
   name: { type: String, required: true },
